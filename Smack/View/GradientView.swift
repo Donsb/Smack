d47f8a9b2a432ab46056befd69de30ffff869b30
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-06 10:28:28 -0800

    GradientView: created new Cocoa Touch file in View Group Folder.  Created class GradientView for our ChannelVC background colour.  Added Inspectable so we can see it on our storyboard.  Added 2 UI Colours.  Added Function LayoutSubviews.

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,60 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Donald Belliveau on 2017-12-06.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    /*
+     Instance Variables
+     */
+    
+    // First colour to use.
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    // Second colour to use.
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    
+    /*
+     Functions
+     */
+    
+    
+    // Layout Subviews Function
+        //-> Tell it what to do when we set our background colour.
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        
+        // Set our two colours
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        
+        // Start start and end point
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        
+        // Set the Size of our GradientLayer to the size of the UIView
+        gradientLayer.frame = self.bounds
+        
+        // Add the gradient Layer to the UIViews layers.
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    
+}
+
+// GradientView: 
