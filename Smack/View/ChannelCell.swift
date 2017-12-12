ea48058b9b75d5cdae483fbffca42e4e1e3431e1
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-12 09:47:34 -0800

    // ChannelCell: Created new class file in View group folder called ChannelCell.  Changed TableViewCell in Storyboard to ChannelCell class.  Added IBOutlets

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,59 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Donald Belliveau on 2017-12-12.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    /*
+     IBOutlets
+     */
+    
+    @IBOutlet weak var channelName: UILabel!
+    
+    /*
+     Functions
+     */
+    
+    
+    // Awake From Nib Function
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    // Set Selected Function.
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+}
+
+
+
+
+
+// ChannelCell: 
+
+
+
+
+
+
+
+
+
+
+
+
+
